45abe64b27fed3910d0fb0b68ffa3a87248021ad
Kristi <kristi.martiko@fshnstudent.info>
2021-05-13 00:11:36 +0200

    UserController Created

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function deleteUser($id) {
+        DB::table('user')->where('user_id', '=', $id)->delete();
+        DB::table('user_role')->where('user_id', '=', $id)->delete();
+    }
+
+    public function getUsers() {
+        return DB::table('user')->select('user.*')->get();
+    }
+
+    public function updateUser(Request $request, $user_id) {
+        $user = User::find($user_id);
+        $user->update($request->all());
+        return $user;
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProduktController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ShporteController;
+use App\Http\Controllers\UserController;
 
 
 /*
@@ -22,19 +23,27 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//product api
 Route::get('/getAllProducts', [ProduktController::class, 'getAllProducts']);
 Route::post('/addProduct', [ProduktController::class, 'addProduct']);
 Route::delete('/delete/{id}', [ProduktController::class, 'destroy']);
 Route::put('/update/{id}', [ProduktController::class, 'update']);
 
+//shporte api
 Route::post('/addToCart/{id}', [ShporteController::class, 'shtoNeShporte']);
 Route::post('/shtoSasi/{id}', [ShporteController::class, 'shtoSasi']);
 Route::post('/hiqSasi/{id}', [ShporteController::class, 'hiqSasi']);
 Route::delete('/cartDelete/{id}', [ShporteController::class, 'fshij']);
 Route::post('/purchase', [ShporteController::class, 'purchase']);
 
+//login and register api
 Route::post('/register', [LoginController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::get('/isAdmin', [LoginController::class, 'isAdmin']);
 Route::get('/getActualUser', [LoginController::class, 'getActualUser']);
 Route::post('/logout', [LoginController::class, 'logout']);
+
+//user api
+Route::get('/getUsers', [UserController::class, 'getUsers']);
+Route::put('/updateUser/{id}', [UserController::class, 'updateUser']);
+Route::delete('/deleteUser/{id}', [UserController::class. 'deleteUser']);
